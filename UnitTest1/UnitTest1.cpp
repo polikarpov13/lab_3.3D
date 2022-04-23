84df1c8fff3ebd403e243a4a7f535dcc0992492a
{Vlad} <{vladpolikarpovvl@gmail.com}>
2022-04-23 17:18:34 +0300

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../../../../../політех/ооп/3/lab_3.3D/lab_3.3D/Triad.cpp"
+#include "../../../../../політех/ооп/3/lab_3.3D/lab_3.3D/Date_Open.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Date_Open d(1,2,3);
+			Assert::AreEqual(2, d.getSecond());
+		}
+	};
+}
---
+++ lab_3.3D/lab_3.3D.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <windows.h>
+#include "Date_Open.h"
+#include "Date_Closed.h"
+
+using namespace std;
+
+int main()
+{
+    Date_Open do_1, do_2;
+
+    cin >> do_1;
+    cout << do_1;
+
+    cin >> do_2;
+    cout << do_2;
+
+    cout << "DO_1 > DO_2";
+    if (do_1 > do_2)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+
+    cout << "DO_1 < DO_2";
+    if (do_1 < do_2)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+
+    Date_Closed dc_1, dc_2;
+
+    cin >> dc_1;
+    cout << dc_1;
+
+    cin >> dc_2;
+    cout << dc_2;
+
+    cout << "DC_1 > DC_2";
+    if (dc_1 > dc_2)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+
+    cout << "DC_1 < DC_2";
+    if (dc_1 < dc_2)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
